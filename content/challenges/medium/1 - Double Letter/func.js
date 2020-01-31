4c9b519de6f9a092e95a398749a77e1321d89506
Omar Fallon <omarfallon@gmail.com>
2020-01-30 20:34:23 -0600

    func in progress

--- content/challenges/medium/1 - Double Letter/func.js
+++ content/challenges/medium/1 - Double Letter/func.js
@@ -1,3 +1,15 @@
 // Create a function that takes a word and returns true if the word has two consecutive identical letters.
 module.exports = (word) => {
+    let lastletter;
+    for (let i = 0; i < word.length; i++){
+        const letter = word[i];
+        if (lastletter) {
+            if (letter === lastletter) {
+                return true;
+            }
+        }
+        lastletter = letter;
+    }
+    return false;
+
 }
